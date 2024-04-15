17ca189d664d923e75d451319f11882fb1236c84
sanika6969 <139212743+sanika6969@users.noreply.github.com>
2024-04-15 12:46:49 +0530

    main

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,29 +31,18 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Model-Based-Imputation",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Imputed MICE Algorithm for missing values",
+    url: "https://github.com/sanika6969/Model-Based-Imputation",
   },
   {
-    title: "Web Development for Beginners",
+    title: "JWT-Authentication",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Implemented a Java web token using a security secret access key",
+    url: "https://github.com/sanika6969/Project-2-JWT-Authentiction",
   },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+ 
 ];
 
 const Portfolio = () => {
